e85601b152c570ebae58c20f48935a2093e25c14
bernizv <vrberniz@gmail.com>
2020-09-06 20:54:34 -0300

    criando validacoes

--- app/src/main/java/com/example/continuada1/MainActivity.kt
+++ app/src/main/java/com/example/continuada1/MainActivity.kt
@@ -11,9 +11,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun Cadastrar (componente:View){
-        Toast.makeText(this, "Cadastrado com Sucesso!", Toast.LENGTH_SHORT).show()
+    fun Cadastrar (componente:View) {
+        when {
+            text.text.isEmpty() || text2.text.isEmpty() || text3.text.isEmpty() -> {
+                Toast.makeText(this, "Obrigatório preencher todos os campos", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
 
-        
+        val email = text.text.toString()
+        val priSenha = text2.text.toString().toInt()
+        val segSenha = text3.text.toString().toInt()
+
+        when{
+            email == "@gmail.com" -> {
+                Toast.makeText(this, "Digite seu e-mail completo!", Toast.LENGTH_SHORT).show()
+                return
+            }
+            priSenha > 6 ->{
+                Toast.makeText(this, "A senha deve conter mais de 6 caracteres!", Toast.LENGTH_SHORT).show()
+                return
+            }
+            segSenha == priSenha -> {
+                Toast.makeText(this, "As senhas devem ser iguais!", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
     }
 }
