43e42409736208e7ac13a8564b38d719ee01ef0d
bernizv <vrberniz@gmail.com>
2020-09-06 20:16:49 -0300

    atualizacao ligação dos campos - layout

--- app/src/main/java/com/example/continuada1/MainActivity.kt
+++ app/src/main/java/com/example/continuada1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.continuada1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun Cadastrar (componente:View){
+        Toast.makeText(this, "Cadastrado com Sucesso!", Toast.LENGTH_SHORT).show()
+
+        
     }
 }
