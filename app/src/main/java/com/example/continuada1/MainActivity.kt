7bd1cef525f20a900f2142332859e7a425374e20
bernizv <vrberniz@gmail.com>
2020-09-09 17:58:41 -0300

    ultima atualização

--- app/src/main/java/com/example/continuada1/MainActivity.kt
+++ app/src/main/java/com/example/continuada1/MainActivity.kt
@@ -11,10 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun Cadastrar (componente:View) {
+    fun cadastrar (componente:View) {
         when {
             text.text.isEmpty() || text2.text.isEmpty() || text3.text.isEmpty() -> {
                 Toast.makeText(this, "Obrigatório preencher todos os campos", Toast.LENGTH_SHORT).show()
+                return
+            }
+            text2.text.length < 6 || text3.text.length < 6 -> {
+                Toast.makeText(this, "A senha deve conter mais de 6 caracteres!", Toast.LENGTH_SHORT).show()
                 return
             }
         }
@@ -23,15 +27,11 @@ class MainActivity : AppCompatActivity() {
         val segSenha = text3.text.toString().toInt()
 
         when{
-
-            priSenha < 6 ->{
-                Toast.makeText(this, "A senha deve conter mais de 6 caracteres!", Toast.LENGTH_SHORT).show()
-                return
-            }
-            segSenha == priSenha -> {
+            segSenha != priSenha -> {
                 Toast.makeText(this, "As senhas devem ser iguais!", Toast.LENGTH_SHORT).show()
                 return
             }
         }
+        Toast.makeText(this, "Cadastrado com sucesso!", Toast.LENGTH_LONG).show()
     }
 }
